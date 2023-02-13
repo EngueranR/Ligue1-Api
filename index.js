c6b96f4525a76fb3532a2025e05fdf45346dc4b0
GuerHat <75025343+EngueranR@users.noreply.github.com>
2023-02-13 12:17:53 +0100

    Fix chrome

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 process.setMaxListeners(0);
 
-let chrome = {};
-let puppeteer;
 const fs = require("fs")
+const puppeteer = require('puppeteer-core');
+const chrome = require('chrome-aws-lambda');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-//const cron = require("node-cron");
 
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    chrome = require("chrome-aws-lambda");
-    puppeteer = require("puppeteer-core");
-} else {
-    puppeteer = require("puppeteer");
-}
+//const cron = require("node-cron");
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     options = {
@@ -25,6 +19,7 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
         timeout: 300000,
     };
 }
+
 
 // cron.schedule(' */5 * * * *', () => {
 
@@ -73,7 +68,15 @@ const urlClassement = 'https://www.ligue1.fr/classement';
 const selectorClassement = '.GeneralStats-row';
 (async function () {
     try {
-        let browser = await puppeteer.launch(options);
+        const browser = await puppeteer.launch(
+            process.env.NODE_ENV === 'production'
+                ? {
+                    args: chrome.args,
+                    executablePath: await chrome.executablePath,
+                    headless: chrome.headless,
+                }
+                : {}
+        );
         const page = await browser.newPage();
         await page.goto(urlClassement, {timeout: 300000});
         const classement = await page.$$eval(selectorClassement, nodes => {
@@ -99,7 +102,15 @@ const getJournee = async (j) => {
     let urlJournee = "https://www.ligue1.fr/calendrier-resultats?matchDay=" + j;
     const selectorJournee = ".match-result";
     try {
-        let browser = await puppeteer.launch(options);
+        const browser = await puppeteer.launch(
+            process.env.NODE_ENV === 'production'
+                ? {
+                    args: chrome.args,
+                    executablePath: await chrome.executablePath,
+                    headless: chrome.headless,
+                }
+                : {}
+        );
         const page = await browser.newPage();
         await page.goto(urlJournee, {timeout: 300000});
         const matchs = await page.$$eval(selectorJournee, (nodes, journee) => {
@@ -135,8 +146,7 @@ const getJournee = async (j) => {
             err => (err ? console.log(err) : null)
         );
         await browser.close();
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
         return null;
     }
